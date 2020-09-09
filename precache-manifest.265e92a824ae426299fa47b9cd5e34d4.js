5c9a489e66a0f43a0d71f2af4f755cb3ccb28cff
Yaroslav Lesyk <slaviklesyk706@gmail.com>
2020-09-09 11:37:35 +0300

    Updates

---
+++ precache-manifest.265e92a824ae426299fa47b9cd5e34d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863fe0159d7f1667c84b66ab424dfa38",
+    "url": "/MTproject/index.html"
+  },
+  {
+    "revision": "f4b7f72209daaa9cff55",
+    "url": "/MTproject/static/css/main.af31f313.chunk.css"
+  },
+  {
+    "revision": "26f37861d45d1fe10369",
+    "url": "/MTproject/static/js/2.35788c4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MTproject/static/js/2.35788c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b7f72209daaa9cff55",
+    "url": "/MTproject/static/js/main.972e927b.chunk.js"
+  },
+  {
+    "revision": "6b74ca20bf13be5bb0ce",
+    "url": "/MTproject/static/js/runtime-main.2e3ae700.js"
+  },
+  {
+    "revision": "54f68190ad41fb2689b9fac8ea425253",
+    "url": "/MTproject/static/media/logo.54f68190.svg"
+  }
+]);
